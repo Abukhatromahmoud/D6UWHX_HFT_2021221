c964f60b74edfe6586d4f1c736e2fca7fd7cacc4
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-10-26 11:45:55 +0200

    alnumrepo

---
+++ D6UWHX_HFT_2021221.Repository/AlbumRepository.cs
@@ -0,0 +1,32 @@
+﻿using D6UWHX_HFT_2021221.Data;
+using D6UWHX_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Repository
+{
+    public class AlbumRepository : Repository<Album>, IAlbumRepository
+    {
+        public AlbumRepository(MusicLibraryContext MusicLibraryContext) : base (MusicLibraryContext)
+        {
+
+        }
+
+        public Album GetByTitle(string title)
+        {
+            return MusicLibraryContext
+                 .Albums
+                 .SingleOrDefault(album => album.Title == title);
+        }
+
+        public override Album GetOne(int id)
+        {
+            return MusicLibraryContext
+                 .Albums
+                 .SingleOrDefault(album => album.AlbumID == id);
+        }
+    }
+}
