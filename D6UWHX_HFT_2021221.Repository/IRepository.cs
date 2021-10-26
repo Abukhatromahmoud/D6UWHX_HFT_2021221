facce98464cbbb6b0355ade7d416221ce36cd9b7
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-10-26 11:49:44 +0200

    Main Repo interface

---
+++ D6UWHX_HFT_2021221.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace D6UWHX_HFT_2021221.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+            T GetOne(int id);
+            IQueryable<T> GetAll();
+            void Add(T entity);
+            void Update(T entity);
+            void Delete(T entity);
+    }
+}
