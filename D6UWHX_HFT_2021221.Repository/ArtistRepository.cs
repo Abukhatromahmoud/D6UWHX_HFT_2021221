4a3eaa3e3a37faa1bb23a399d339d49d08858b57
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-10-26 11:46:24 +0200

    artist repo

---
+++ D6UWHX_HFT_2021221.Repository/ArtistRepository.cs
@@ -0,0 +1,27 @@
+﻿using D6UWHX_HFT_2021221.Data;
+using D6UWHX_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Repository
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(MusicLibraryContext musicLibraryContext) : base (musicLibraryContext    )
+        {
+
+        }
+
+        public override Artist GetOne(int id)
+        {
+            return MusicLibraryContext
+                .Artists
+                .SingleOrDefault(artist => artist.ArtistId == id);
+        
+        }
+
+    }
+}
