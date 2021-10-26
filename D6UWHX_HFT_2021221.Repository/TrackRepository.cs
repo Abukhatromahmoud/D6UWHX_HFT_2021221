e10de2620c9f2317de3b461eca92539d2989a2ce
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-10-26 11:49:23 +0200

    Track Repo

---
+++ D6UWHX_HFT_2021221.Repository/TrackRepository.cs
@@ -0,0 +1,26 @@
+﻿using D6UWHX_HFT_2021221.Data;
+using D6UWHX_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Repository
+{
+    public class TrackRepository : Repository<Track> , ITrackRepository  
+    {
+        public TrackRepository(MusicLibraryContext  musicLibraryContext ) : base(musicLibraryContext)
+        {
+                
+        }
+
+        public override Track GetOne(int id)
+        {
+            return MusicLibraryContext
+                .Tracks
+                .SingleOrDefault(x => x.TrackId == id); 
+        }
+
+    }
+}
