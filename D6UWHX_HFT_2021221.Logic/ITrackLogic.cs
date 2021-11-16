5d5250b945affba86401ee4ed4433b7e98074f8e
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-11-16 11:49:46 +0100

    TrackLogicCRUDmethod

---
+++ D6UWHX_HFT_2021221.Logic/ITrackLogic.cs
@@ -0,0 +1,19 @@
+﻿using D6UWHX_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Logic
+{
+    public interface ITrackLogic
+    {
+        List<Track> GetTracks();
+        Track GetTrack(int TrackId);
+        void CreatTrack(int TrackId, string NamePlace, int Length);
+        void UpdateTrack(Track track);
+        void DeleteTrack(int TrackId);
+
+    }
+}
---
+++ D6UWHX_HFT_2021221.Logic/TrackLogic.cs
@@ -0,0 +1,69 @@
+﻿using D6UWHX_HFT_2021221.Models;
+using D6UWHX_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Logic
+{
+    public class TrackLogic : ITrackLogic
+    {
+        private readonly ITrackRepository _trackRepository;
+        public TrackLogic(ITrackRepository trackRepository)
+        {
+            _trackRepository = trackRepository;
+        }
+
+        public void CreatTrack(int trackId, string namePlace, int length)
+        {
+            Track track = new Track
+            {
+                TrackId = trackId,
+                NamePlace = namePlace,
+                Length = length
+
+            };
+            _trackRepository.Add(track);
+        }
+
+        public void DeleteTrack(int trackId)
+        {
+            Track track = _trackRepository.GetOne(trackId);
+            if (track == null )
+            {
+                throw new Exception("NOt valid Track Id ");
+            }
+            _trackRepository.Delete(track);
+        }
+
+        public Track GetTrack(int TrackId)
+        {
+            Track track = _trackRepository.GetOne(TrackId);
+            if (track == null )
+            {
+                throw new Exception("Not Valid Artist Id ");
+            }
+            return track;
+        }
+
+        public List<Track> GetTracks()
+        {
+            return _trackRepository.GetAll().ToList();
+        }
+
+        public void UpdateTrack(Track track )
+        {
+            Track currentTrack = _trackRepository.GetOne(track.TrackId);
+            if (currentTrack == null)
+            {
+                throw new Exception("Not Existing ");
+            }
+            currentTrack.Album = track.Album;
+            currentTrack.Length = track.Length;
+            currentTrack.NamePlace = track.NamePlace;
+            _trackRepository.Update( currentTrack);        
+        }
+    }
+    }
