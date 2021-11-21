67989d2f6b523af95afc17af957f1d3a2ed6fed8
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-11-21 02:20:52 +0100

    Test F

---
+++ D6UWHX_HFT_2021221.Test/LogicLayerTest.cs
@@ -0,0 +1,21 @@
+﻿using D6UWHX_HFT_2021221.Logic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6UWHX_HFT_2021221.Test
+{
+    [TestFixture]
+    public class LogicLayerTest
+    {
+        private AlbumLogic AlbumLogic { get; set; }
+        public ArtistLogic ArtistLogic { get; set; }
+        public TrackLogic TrackLogic { get; set; }
+      
+       
+
+    }
+}
