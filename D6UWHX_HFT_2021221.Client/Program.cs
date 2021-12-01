2244610c21abf6c3b801ac982d34aefc5e4d6e4c
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2021-12-01 01:42:01 +0100

    asdas

--- D6UWHX_HFT_2021221.Client/Program.cs
+++ D6UWHX_HFT_2021221.Client/Program.cs
@@ -5,7 +5,7 @@ using D6UWHX_HFT_2021221.Models;
 using D6UWHX_HFT_2021221.Repository;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace D6UWHX_HFT_2021221.Client
 {
@@ -13,22 +13,22 @@ namespace D6UWHX_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            System.Threading.Thread.Sleep(8000);
+            //System.Threading.Thread.Sleep(8000);
 
-            RestService rest = new RestService("http://localhost:7000");
+            //RestService rest = new RestService("https://go.microsoft.com/fwlink/?LinkID=398940");
 
-            rest.Post<Track>(new Track()
-            {
-                NamePlace = "ballads"
-            }, "ballads");
+            //rest.Post<Track>(new Track()
+            //{
+            //    NamePlace = "ballads"
+            //}, "ballads");
 
-            var tracks = rest.Get<Track>("novelty songs");
-            var tracks1 = rest.Get<Track>("rock");
+            //var tracks = rest.Get<Track>("novelty songs");
+            //var tracks1 = rest.Get<Track>("rock");
 
-            double avgprice = rest.GetSingle<double>("stat/avgprice");
+            //double avgprice = rest.GetSingle<double>("stat/avgprice");
 
-            var avgpricebybrands = rest
-                .Get<KeyValuePair<string, double>>("stat/avgpricebybrands");
+            //var avgpricebybrands = rest
+            //    .Get<KeyValuePair<string, double>>("stat/avgpricebybrands");
             
             //
             MusicLibraryContext mlc = new MusicLibraryContext();
@@ -39,7 +39,7 @@ namespace D6UWHX_HFT_2021221.Client
             AlbumLogic albumLogic = new AlbumLogic(albumRepo);
             ArtistLogic artistLogic = new ArtistLogic(artistRepo);
 
-            var subMenuCustomerCreate = new ConsoleMenu()
+            var subMenuCreate = new ConsoleMenu()
                  .Add(">>ADD A NEW TRACK", () => AddNewTrack(trackLogic))
                  .Add(">> CREATE A NEW Album", () => AddNewAlbum(albumLogic))
                  .Add(">> ADD A NEW Artist TO the music", () => AddNewArtist(artistLogic))
@@ -49,15 +49,64 @@ namespace D6UWHX_HFT_2021221.Client
                      config.Selector = "--> ";
                      config.SelectedItemBackgroundColor = ConsoleColor.Green;
                  });
-            var menu = new ConsoleMenu()
-               .Add(">> ENTER AS A Track", () => subMenuCustomerCreate.Show())
-               .Add(">> EXIT", ConsoleMenu.Close)
+            //
+            var subMenuList = new ConsoleMenu()
+                .Add(">> LIST ALL Tracks", () => ListAllTracks(trackLogic))
+                .Add(">> LIST ALL ALBums", () => ListAllAlbum(albumLogic))
+                .Add(">> LIST ALL ARTIST", () => ListAllArtist(artistLogic))
+
+                .Add(">> GO BACK TO MENU", ConsoleMenu.Close)
+                .Configure(config =>
+                {
+                    config.Selector = "--> ";
+                    config.SelectedItemBackgroundColor = ConsoleColor.Green;
+                });
+            var subMenuGetBy = new ConsoleMenu()
+               .Add(">> GET ONE TRACK BY ID", () => GetOneTrack(trackLogic))
+               .Add(">> GET ONE ALBUM BY ID", () => GetOneAlbum(albumLogic))
+               .Add(">> GET ONE ARTIST BY ID", () => GetOneArtist(artistLogic))
+               .Add(">> GO BACK TO MENU", ConsoleMenu.Close)
                .Configure(config =>
                {
                    config.Selector = "--> ";
-                   config.SelectedItemBackgroundColor = ConsoleColor.Cyan;
+                   config.SelectedItemBackgroundColor = ConsoleColor.DarkYellow;
                });
 
+            var subMenuListRead = new ConsoleMenu()
+               .Add(">> LIST ALL", () => subMenuList.Show())
+               .Add(">> GET BY ID", () => subMenuGetBy.Show())
+               .Add(">> GO BACK TO MENU", ConsoleMenu.Close)
+               .Configure(config =>
+               {
+                   config.Selector = "--> ";
+                   config.SelectedItemBackgroundColor = ConsoleColor.Green;
+               });
+
+
+            var subMenuMusic = new ConsoleMenu()
+            .Add(">> C - CREATE", () => subMenuCreate.Show())
+            //.Add(">> R - READ", () => subMenuCompanyRead.Show())
+            //.Add(">> U - UPDATE", () => subMenuCompanyUpdate.Show())
+            //.Add(">> D - DELETE", () => subMenuCompanyDelete.Show())
+            //.Add(">> NON-CRUD - QUERIES", () => subMenuCompanyNonCrud.Show())
+            .Add(">> GO BACK TO MENU", ConsoleMenu.Close)
+            .Configure(config =>
+            {
+                config.Selector = "--> ";
+                config.SelectedItemBackgroundColor = ConsoleColor.DarkYellow;
+            });
+
+
+
+            var menu = new ConsoleMenu()
+              .Add(">> ENTER AS A VISITOR ", () => subMenuMusic.Show())
+              
+              .Add(">> EXIT", ConsoleMenu.Close)
+              .Configure(config =>
+              {
+                  config.Selector = "--> ";
+                  config.SelectedItemBackgroundColor = ConsoleColor.Cyan;
+              });
 
             menu.Show();
 
@@ -117,8 +166,107 @@ namespace D6UWHX_HFT_2021221.Client
 
             Console.ReadLine();
         }
+        private static void ListAllTracks(TrackLogic trackLogic)
+        {
+            Console.WriteLine("\n:: ALL Tracks ::\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            
+            Console.ResetColor();
+            trackLogic.GetTracks().ToList().ForEach(x => Console.WriteLine(x.ToString()));
+            Console.ReadLine();
+        }
+        private static void ListAllArtist(ArtistLogic artistLogic)
+        {
+            Console.WriteLine("\n:: ALL Artist ::\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.ResetColor();
+            artistLogic.GetArtists().ToList().ForEach(x => Console.WriteLine(x.ToString()));
+            Console.ReadLine();
+        }
+        private static void ListAllAlbum(AlbumLogic albumLogic)
+        {
+            Console.WriteLine("\n:: ALL Album ::\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.ResetColor();
+            albumLogic.GetAlbums().ToList().ForEach(x => Console.WriteLine(x.ToString()));
+            Console.ReadLine();
+        }
+        private static void GetOneTrack(TrackLogic trackLogic)
+        {
+            Console.WriteLine("\n:: TYPE THE ID OF THE TRACK YOU WANT TO SEE ::\n");
+            try
+            {
+                int id = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ResetColor();
+                Console.WriteLine(trackLogic.GetTrack(id).ToString());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+        private static void GetOneAlbum(AlbumLogic albumLogic)
+        {
+            Console.WriteLine("\n:: TYPE THE ID OF THE ALBUM YOU WANT TO SEE ::\n");
+            try
+            {
+                int id = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ResetColor();
+                Console.WriteLine(albumLogic.GetAlbum(id).ToString());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+        private static void GetOneArtist(ArtistLogic artistLogic)
+        {
+            Console.WriteLine("\n:: TYPE THE ID OF THE ARTIST YOU WANT TO SEE ::\n");
+            try
+            {
+                int id = int.Parse(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.ResetColor();
+                Console.WriteLine(artistLogic.GetArtist(id).ToString());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+        private static void GetShortestTrack(TrackLogic trackLogic)
+        {
+            Console.WriteLine("\n:: I WILL GIVE YOU THE SHORTEST TRACK ::\n");
+            var item = trackLogic.GetShortestTrack();
+            
+        }
+
+
+
     }
-    
+
 }
 
     
