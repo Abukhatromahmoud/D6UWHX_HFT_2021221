dd404b7d3d4fdaef50dd5a1f916879648e9d3bf8
Abukhatromahmoud <67956393+Abukhatromahmoud@users.noreply.github.com>
2022-05-15 15:39:12 +0200

    Delete Documentation/Doxygen directory

--- Documentation/Doxygen/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['homecontroller_187',['HomeController',['../class_my_tobacco_shop_1_1_web_1_1_controllers_1_1_home_controller.html',1,'MyTobaccoShop::Web::Controllers']]]
-];
--- Documentation/Doxygen/search/classes_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['startup_221',['Startup',['../class_my_tobacco_shop_1_1_web_1_1_startup.html',1,'MyTobaccoShop::Web']]]
-];
